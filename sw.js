7eda3fc0afe0662398fda13d070c6cd611ec93e3
realfakenerd <lucas210898@hotmail.com>
2021-12-10 02:22:43 -0300

    Gerado automáticamente pelo ghPages

--- sw.js
+++ sw.js
@@ -1,14 +1,27 @@
-import {
-    pageCache,
-    imageCache,
-    googleFontsCache,
-    staticResourceCache,
-  } from "workbox-recipes";
-  import { clientsClaim } from "workbox-core";
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.2/workbox-sw.js');
+if (workbox) {
+    workbox.setConfig({
+        debug: false
+    });
+    const wkb = {
+        ...workbox.core,
+        ...workbox.recipes
+    }
+    const {
+        clientsClaim,
+        pageCache,
+        staticResourceCache,
+        googleFontsCache,
+        imageCache
+    } = wkb;
 
-  clientsClaim();
-
-  pageCache();
-  googleFontsCache();
-  imageCache();
-  staticResourceCache();
+    imageCache();
+    pageCache();
+    googleFontsCache();
+    staticResourceCache({
+        cacheName: 'assets'
+    });
+    clientsClaim();
+} else {
+    console.log('Boo! Workbox failed to load 😬');
+}
