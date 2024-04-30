55ce26cc2f624a74e8c6db3f87720c976a5b6c6d
Lucas A. Ouverney <realfakenerd@users.noreply.github.com>
2024-04-30 19:38:26 -0300

    Update +layout.ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,7 @@
 import type { LayoutLoad } from "./$types"
 
 export const load = (async ({url, fetch}) => {
-    const res = await fetch('/api/pokemon?limit=1302');
+    const res = await fetch('/api/pokemon?limit=100');
 	const pokemones = (await res.json()) as CachedPokemonList;
     return {
         currentPath: url.pathname,
